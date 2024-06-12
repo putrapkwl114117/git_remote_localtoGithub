03bc7b8e971cfc3940de2953e9a517014c211d31
putrapkwl114117 <putrawork7@gmail.com>
2024-06-13 02:50:37 +0700

    menambahkan file style.css

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <h1>Mencoba push dari lcal ke github</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, aspernatur quos earum officia exercitationem
