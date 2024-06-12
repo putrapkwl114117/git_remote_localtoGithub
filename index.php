4b97398004f25f7de890f8c53e6f92df1a870776
putrapkwl114117 <putrawork7@gmail.com>
2024-06-13 02:43:31 +0700

    menambahkan list baru ke index.php

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 <h1>Mencoba push dari lcal ke github</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, aspernatur quos earum officia exercitationem
     excepturi eius dolorem nam corporis cumque iusto ullam rerum omnis quibusdam id, quas minus molestiae dolores?</p>
+<ul>
+    <li>putra pongkowulu : 123</li>
+    <li>putra pongkowulu : 321</li>
+</ul>
 
 <body>
 
