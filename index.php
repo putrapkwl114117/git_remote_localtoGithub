680ee7d67d84bd7c5f272bc1911167b3d44b92a5
putrapkwl114117 <putrawork7@gmail.com>
2024-06-13 02:30:22 +0700

    menambhakan paragraf ke file index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <title>Document</title>
 </head>
 <h1>Mencoba push dari lcal ke github</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, aspernatur quos earum officia exercitationem
+    excepturi eius dolorem nam corporis cumque iusto ullam rerum omnis quibusdam id, quas minus molestiae dolores?</p>
 
 <body>
 
